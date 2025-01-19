408d3087a1c7cfdab269bc4d32feb930ff8aca54
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-19 22:02:57 +0530

    Power Calculation

--- PowerCal.java
+++ PowerCal.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PowerCal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Base: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter Exponent: ");
+        double exponent = sc.nextDouble();
+        double result = Math.pow(base, exponent);
+        System.out.println("Result: " + result);
+
+    }
 }
