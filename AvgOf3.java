4a40a443816fbde9d27caff2dc0a9c66856e8449
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-19 22:03:42 +0530

    Calculate Average of Three Numbers

--- AvgOf3.java
+++ AvgOf3.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class AvgOf3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter Second Number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Enter Third Number: ");
+        double num3 = scanner.nextDouble();
+        double average = (num1 + num2 + num3) / 3;
+        System.out.println("Average: " + average);
+    }
 }
