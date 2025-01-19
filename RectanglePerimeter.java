1e14fc454a74eeb3f82052e59bd235777e967f53
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-19 22:02:01 +0530

    Perimeter of a Rectangle

--- RectanglePerimeter.java
+++ RectanglePerimeter.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class RectanglePerimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Length: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter Width: ");
+        double width = sc.nextDouble();
+        double perimeter = 2 * (length + width);
+        System.out.println("Perimeter of the Rectangle: " + perimeter);
+
+    }
 }
