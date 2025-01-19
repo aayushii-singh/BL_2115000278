733467e338fb0696a36c6554fdf4f2f14d6bdedb
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-19 22:04:22 +0530

    Convert Kilometers to Miles

--- kmToMiles.java
+++ kmToMiles.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class kmToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Distance in Kilometers: ");
+        double km = sc.nextDouble();
+        double miles =km * 0.621371;
+        System.out.println("Distance in Miles: " + miles);
+
+    }
 }
