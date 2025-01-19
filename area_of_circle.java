105417b5066e96cfc9bd984e1ea95377e69819c6
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-01-19 21:41:47 +0530

    area

---
+++ area_of_circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of a circle: " + area);
+    }
+}
